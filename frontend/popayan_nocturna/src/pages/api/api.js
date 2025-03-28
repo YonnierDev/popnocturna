49a4eee6dc01f6cdb2f2b715adcbe211d631faf0
YonnierDev <yonnierleon@gmail.com>
2025-03-27 20:49:47 -0500

    Delet file frontend

--- frontend/popayan_nocturna/src/pages/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: "", // Asegúrate de que el backend usa HTTPS
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
