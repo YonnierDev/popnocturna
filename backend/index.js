aff3163c543136fd2f87279490eb5d368a3d828c
YonnierDev <yonnierleon@gmail.com>
2025-03-31 16:57:28 -0500

    package.json

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,9 @@
 require("dotenv").config();
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
 const express = require("express");
 const cors = require("cors");
 
