e53250a5d70f189b4513db31b2b958180537dca4
YonnierDev <yonnierleon@gmail.com>
2025-03-31 21:27:58 -0500

    cors

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,13 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+
 
 // Rutas
 app.use("/api", require("./routes/rolRouter"));
