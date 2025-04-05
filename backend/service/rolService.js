d5071f05a7141c82acc0081a6e159ba3b5519e43
YonnierDev <yonnierleon@gmail.com>
2025-04-05 08:27:41 -0500

    mejoras en las relaciones

--- backend/service/rolService.js
+++ backend/service/rolService.js
@@ -1,21 +1,27 @@
-const {Rol}=require('../models')
-class RolService{
-    async listarRol() {
-        return await Rol.findAll({
-            include: [{ model: Usuario, as: "usuarios", attributes: ["id", "nombre"]}]
-        });
-    }
-    async crearRol(nombre) {
-        return await Rol.create({nombre})
-    }
-    async actualizarRol(id,nombre) {
-        return await Rol.update({nombre}, { where: { id } });      
-    }
-    async eliminarRol(id) {
-        return await Rol.destroy({ where: { id } });
-    }
-    async buscarRol(id) {
-        return await Rol.findByPk(id);
-    }
+const { Rol, Usuario } = require("../models");
+class RolService {
+  async listarRol() {
+    return await Rol.findAll({
+      include: [
+        {
+          model: Usuario,
+          as: "usuarios",
+          attributes: ["id", "nombre"],
+        },
+      ],
+    });
+  }
+  async crearRol(nombre) {
+    return await Rol.create({ nombre });
+  }
+  async actualizarRol(id, nombre) {
+    return await Rol.update({ nombre }, { where: { id } });
+  }
+  async eliminarRol(id) {
+    return await Rol.destroy({ where: { id } });
+  }
+  async buscarRol(id) {
+    return await Rol.findByPk(id);
+  }
 }
 module.exports = new RolService();
