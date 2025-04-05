406527a0336cace609ff54aae6771cb3a81fef1d
YonnierDev <yonnierleon@gmail.com>
2025-04-05 10:55:18 -0500

    UsuarioController-crear

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -4,6 +4,7 @@ const UsuarioController = require("../controllers/usuarioController");
 
 router.get("/usuarios", UsuarioController.listarUsuarios);
 router.get("/usuario/:id", UsuarioController.buscarUsuario);
+router.post("/usuario", UsuarioController.crearUsuario);
 router.put("/usuario/:id", UsuarioController.actualizarUsuario);
 router.delete("/usuario/:id", UsuarioController.eliminarUsuario);
 router.get("/usuarios/relaciones", UsuarioController.listarRelacionesUsuarios);
