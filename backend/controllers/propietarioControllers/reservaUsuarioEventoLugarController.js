bf49aab97d8a2cb37a39e511f51867f4c83d1353
YonnierDev <yonnierleon@gmail.com>
2025-04-13 22:40:33 -0500

    Middlewatre valida por rol

--- backend/controllers/propietarioControllers/reservaUsuarioEventoLugarController.js
+++ backend/controllers/propietarioControllers/reservaUsuarioEventoLugarController.js
@@ -1,14 +1,15 @@
-const reservaUsuarioEventoLugar = require("../../service/propietarioService/reservaUsuarioEventoLugarService");
+const reservaUsuarioEventoLugarService = require("../../service/propietarioService/reservaUsuarioEventoLugarService");
 
 class ReservaUsuarioEventoLugarController {
-    async listarReservas(req, res) {
-        try {
-          const reservas = await reservaUsuarioEventoLugar.listarReservasConDetalles();
-          res.json(reservas);
-        } catch (error) {
-          res.status(500).json({ mensaje: "Error no lista las reservas ",error });
-        }
-      }
+  async listarReservas(req, res) {
+    try {
+      const usuarioid = req.usuario.id;
+      const reservas = await reservaUsuarioEventoLugarService.listarReservasConDetalles(usuarioid);
+      res.json(reservas);
+    } catch (error) {
+      res.status(500).json({ mensaje: "Error al listar las reservas", error: error.message });
+    }
+  }
 }
 
 module.exports = new ReservaUsuarioEventoLugarController();
--- backend/routes/propietarioRouters/reservaUsuarioEventoLugarRouter.js
+++ backend/routes/propietarioRouters/reservaUsuarioEventoLugarRouter.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const reservaUsuarioEventoLugarController = require('../../controllers/propietarioControllers/reservaUsuarioEventoLugarController');
+const autentiMiddleware = require("../../middlewares/autentiMiddleware"); 
+const validarRol = require("../../middlewares/validarRol"); 
 
-router.get('/reservasdetalle', reservaUsuarioEventoLugarController.listarReservas);
+router.get('/reservasdetalle', autentiMiddleware, validarRol(3), reservaUsuarioEventoLugarController.listarReservas);
 
 module.exports = router;
