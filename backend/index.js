3f16f449a902cf8d9e59d3c73b807f14c32b70b0
YonnierDev <yonnierleon@gmail.com>
2025-06-19 14:25:18 -0500

    Añadir el puerto por defecto de vite para desarrollo local en el front

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,8 @@ app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
   origin: [
     'http://localhost:3000',
-    'https://frontendpopa.vercel.app'
+    'https://frontendpopa.vercel.app',
+    'http://localhost:5173'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
