f229bde48bf0d36c4878006e26ef8b016dff1cc9
ElezDev <elezdev2023@gmail.com>
2025-06-25 11:45:54 -0500

    fix: corregir la ruta del archivo de cuenta de servicio en fcmFirebaseService.js

--- backend/service/fcmFirebase/fcmFirebaseService.js
+++ backend/service/fcmFirebase/fcmFirebaseService.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 const path = require('path');
 
-const serviceAccount = require(path.resolve(__dirname, ''));
+const serviceAccount = require(path.resolve(__dirname, '../../config/nocturnapopayan-admin.json'));
 
 if (!admin.apps.length) {
   admin.initializeApp({
